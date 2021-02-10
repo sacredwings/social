dcc1728f7bf0e2267ca877137fcbd87815eaa22d
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2021-02-10 18:33:14 +0700

    На лейдинг добавлены - телеграмм канал и пожертвование

--- src/js/pages/Landing.js
+++ src/js/pages/Landing.js
@@ -28,9 +28,16 @@ class Landing extends Component {
                             <div className="col-lg-12">
                                 <img src={`/logo_big.png`} style={{maxWidth: '100px'}}/>
                                 <br/>
-                                <button type="button" className="btn btn-light">💵 ..поддержи проэкт.. 💵</button>
+                                {/*<button type="button" className="btn btn-light">💵 ..поддержи проэкт.. 💵</button>*/}
+
+
                             </div>
                         </div>
+
+                        <br/>
+                        <hr/>
+                        <br/>
+
                         <div className="row text-center">
                             <div className="col-lg-12">
                                 <h1 className="">Социальная сеть для военнослужащих</h1>
@@ -43,11 +50,29 @@ class Landing extends Component {
 
                         </div>
 
+                        <br/>
+                        <hr/>
+                        <br/>
+
                         <div className="row text-center">
                             <div className="col-lg-12">
-                                <span className="badge bg-warning text-dark">Запуск сети в течении 2 недель</span>
+                                <img src={'https://avatars.mds.yandex.net/get-zen_doc/1583807/pub_5e58cf33baedf81f7d511363_5e58e391f8544a5bec015ab8/scale_1200'} style={{height: 150}}/>
+                                <h2 className="">Новости сайта в телеграмм канале</h2>
+                                <br/>
+                                <a className="btn btn-primary" href="https://t.me/voenset" role="button">Перейти на канал</a>
                             </div>
 
+                        </div>
+
+                        <br/>
+                        <hr/>
+                        <br/>
+
+                        <div className="row" style={{width: 'max-content'}}>
+                            <iframe
+                                src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=%D0%9D%D0%B0%20%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D0%B5%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0&targets-hint=&default-sum=100&button-text=11&hint=&successURL=http%3A%2F%2Furpravovoen.ru%2Fnavigation%2F&quickpay=shop&account=4100110634652084"
+                                width="422" height="223" frameBorder="0" allowTransparency="true" scrolling="no">
+                            </iframe>
                         </div>
                     </div>
                 </div>
