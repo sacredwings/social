65124adac9cb8c23ee30aa2baf7e21b36fd65575
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2021-02-20 23:15:11 +0700

    Переписан видео компонент

--- src/js/elements/VideoAddModal.js
+++ src/js/elements/VideoAddModal.js
@@ -1,52 +1,45 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import axios from "axios";
-import {ReCaptcha} from 'react-top-recaptcha-v3';
+import {ReCaptcha} from "react-top-recaptcha-v3";
 import SelectAlbum from "../objects/SelectAlbum";
 
-class VideoAddModal extends Component {
-    constructor () {
-        super();
-        this.state = {
-            file: null,
-            inputTitle: '',
-            inputText: '',
-            processBarLoaded: 0,
-            processBarTotal: 0,
-            processBar: 0,
+function VideoAddModal (props) {
+    let [gtoken, setGtoken] = useState('')
+    let [form, setForm] = useState({
+        file_preview: null,
+        file: null,
+        inputTitle: '',
+        inputText: '',
+        processBarLoaded: 0,
+        processBarTotal: 0,
+        processBar: 0,
 
-            response: {
-                count: 0,
-                items: []
-            },
-            count: 20,
-            offset: 0,
-            arAlbums: [],
+        response: {
+            count: 0,
+            items: []
+        },
+        count: 20,
+        offset: 0,
+        arAlbums: [],
 
-            arSelectAlbums: []
-        }
+        arSelectAlbums: []
+    })
 
-        this.ChangeSelectAlbums = this.ChangeSelectAlbums.bind(this)
-        this.onFormSubmitFile = this.onFormSubmitFile.bind(this)
-        this.onChangeFile = this.onChangeFile.bind(this)
-        this.onChangeText = this.onChangeText.bind(this)
-        this.onFormClose = this.onFormClose.bind(this)
+    let recaptcha;
 
-    }
+    //отслеживаем изменение props
+    useEffect (async () => {
+        await GetAlbums()
+    }, [])
 
     //получаем результат выбранных альбомов от checked
-    ChangeSelectAlbums (arSelectAlbums) {
-        this.setState({arSelectAlbums: arSelectAlbums})
+    const ChangeSelectAlbums = (arSelectAlbums) => {
+        setForm(prev => ({...prev, arSelectAlbums: arSelectAlbums}))
     }
 
-    async componentDidMount () {
-        await this.GetAlbums()
-    }
-
-    async GetAlbums () {
-        let owner_id = this.props.owner_id;
-
-        const url = `/api/video/getAlbums?owner_id=${owner_id}&offset=${this.state.offset}&count=${this.state.count}`;
+    const GetAlbums = async (start) => {
+        const url = `/api/video/getAlbums?owner_id=${props.owner_id}&offset=${form.offset}&count=${form.count}`;
 
         let result = await axios.get(url);
 
@@ -55,59 +48,63 @@ class VideoAddModal extends Component {
 
         if (!result.response) return
 
-        this.setState(prevState => ({
+        setForm(prev => ({...prev, ...{
+            offset: (start) ? 0 : prev.offset + form.count,
+            count: result.response.count,
             response: result.response,
-            arAlbums: [...prevState.arAlbums, ...result.response.items],
-            offset: prevState.offset + prevState.count
+            arAlbums: (start) ? result.response.items : [...prev.arAlbums, ...result.response.items],
+        }}))
+    }
+
+    const onChangeFile = (e) => {
+        let name = e.target.id;
+
+        setForm(prev => ({
+            ...prev, [name]:e.target.files[0]
         }))
     }
 
-    onChangeFile(e) {
-        this.setState({file:e.target.files[0]})
-        console.log(this.state.file)
-    }
-
-    onChangeText(e) {
+    const onChangeText = (e) => {
         let name = e.target.id;
         let value = e.target.value;
 
-        this.setState({[name]:value})
+        setForm(prev => ({
+            ...prev, [name]: value
+        }))
     }
 
-    onFormClose(e) {
+    const onFormClose = (e) => {
 
-        this.setState({
-            file: null,
-            inputTitle: '',
-            inputText: '',
-            processBarLoaded: 0,
-            processBarTotal: 0,
-            processBar: 0
-        })
+        setForm(prev => ({...prev, ...{
+                inputFilePreview: null,
+                inputFile: null,
+                inputTitle: '',
+                inputText: '',
+                processBarLoaded: 0,
+                processBarTotal: 0,
+                processBar: 0
+            }}))
     }
 
-    onFormSubmitFile (e) {
-        let _this = this
-        console.log(this.state.file);
-
-
+    const onFormSubmitFile = (e) => {
         const url = '/api/video/add';
         const formData = new FormData();
 
-        console.log(this.state)
+        console.log(form)
 
-        formData.append('file', this.state.file)
+        formData.append('file_preview', form.inputFilePreview)
+        formData.append('file', form.inputFile)
 
-        formData.append('title', this.state.inputTitle)
-        formData.append('text', this.state.inputText)
-        formData.append('gtoken', this.state.gtoken)
+        formData.append('title', form.inputTitle)
+        formData.append('text', form.inputText)
+        formData.append('gtoken', gtoken)
 
-        if (this.state.arSelectAlbums.length)
-            formData.append('albums', this.state.arSelectAlbums.join(','))
+        if (form.arSelectAlbums.length)
+            formData.append('albums', form.arSelectAlbums.join(','))
 
         //если это группа, то отправляем ее id
-        if ((this.props.owner_id) && (this.props.owner_id<0))
-            formData.append('group_id', -this.props.owner_id)
+        if ((props.owner_id) && (props.owner_id<0))
+            formData.append('group_id', -props.owner_id)
 
         axios.post(url, formData, {
 
@@ -119,7 +116,12 @@ class VideoAddModal extends Component {
                 if (progressEvent.lengthComputable) {
                     let percentage = Math.floor((progressEvent.loaded * 100) / progressEvent.total)
                     console.log(progressEvent.loaded + ' ' + progressEvent.total + ' ' + percentage);
-                    _this.setState({processBar: percentage, processBarLoaded: progressEvent.loaded, processBarTotal: progressEvent.total})
+
+                    setForm(prev => ({...prev, ...{
+                            processBarLoaded: progressEvent.loaded,
+                            processBarTotal: progressEvent.total,
+                            processBar: percentage
+                        }}))
 
                 }
                 // Do whatever you want with the native progress event
@@ -131,84 +133,75 @@ class VideoAddModal extends Component {
         e.preventDefault() // Stop form submit
     }
 
-    render() {
-        return (
-            <div className="modal fade" id="modalVideoAdd" tabIndex="-1" aria-labelledby="modalVideoAdd"
-                 aria-hidden="true">
+    return (
+        <div className="modal fade" id="modalVideoAdd" tabIndex="-1" aria-labelledby="modalVideoAdd"
+             aria-hidden="true">
 
-                <ReCaptcha
-                    ref={ref => this.recaptcha = ref}
-                    action='settings'
-                    sitekey={global.gappkey}
-                    verifyCallback={token => this.setState({gtoken: token})}
-                />
-                <form onSubmit={this.onFormSubmitFile}>
+            <ReCaptcha
+                ref={ref => recaptcha = ref}
+                action='settings'
+                sitekey={global.gappkey}
+                verifyCallback={token => setGtoken(token)}
+            />
+            <form onSubmit={onFormSubmitFile}>
 
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Новое видео</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal"
-                                        aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body">
-
-                                <p>Выберите видео файл на своем устройстве</p>
-                                <div className="mb-3 form-file">
-                                    <input type="file" className="form-file-input" id="inputFile"
-                                           onChange={this.onChangeFile}/>
-                                    <label className="form-file-label" htmlFor="inputFile">
-                                        <span className="form-file-text">Выберите файл...</span>
-                                        <span className="form-file-button">Обзор</span>
-                                    </label>
-                                </div>
-
-                                <div className="mb-3">
-                                    <label htmlFor="" className="form-label">Альбомы</label>
-                                    {/* checked массив альбомов */}
-                                    <SelectAlbum albums={this.state.arAlbums} func={this.ChangeSelectAlbums}/>
-                                </div>
-
-                                <div className="mb-3">
-                                    <label htmlFor="inputTitle" className="form-label">Название</label>
-                                    <input type="text" className="form-control" id="inputTitle"
-                                           onChange={this.onChangeText} value={this.state.inputTitle}/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="inputText" className="form-label">Описание</label>
-                                    <textarea className="form-control" id="inputText" rows="5"
-                                              onChange={this.onChangeText} value={this.state.inputText}></textarea>
-                                </div>
-
-                                {((this.state.processBar > 0) && (this.state.processBar < 100)) ?
-                                    <div className="mb-3"><p className="text-primary">Загружаю</p></div> : null}
-                                {(this.state.processBar === 100) ?
-                                    <div className="mb-3"><p className="text-success">Загружено</p></div> : null}
-                                <div className="progress">
-                                    <div className="progress-bar progress-bar-striped progress-bar-animated"
-                                         role="progressbar" style={{width: `${this.state.processBar}%`}}
-                                         aria-valuenow={this.state.processBar} aria-valuemin="0"
-                                         aria-valuemax="100"></div>
-                                </div>
-
-
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
-                                        onClick={this.onFormClose}>Закрыть
-                                </button>
-                                <button type="submit" className="btn btn-primary"
-                                        disabled={(this.state.processBar !== 0) ? true : false}>Добавить
-                                </button>
-                            </div>
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Новое видео</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
                         </div>
+                        <div className="modal-body">
 
+                            <div className="mb-3">
+                                <label htmlFor="inputFilePreview" className="form-label">Картинка (значек)</label>
+                                <input className="form-control form-control-sm" id="inputFilePreview" type="file" onChange={onChangeFile}/>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="inputFile" className="form-label">Видео</label>
+                                <input className="form-control form-control-sm" id="inputFile" type="file" onChange={onChangeFile}/>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="" className="form-label">Альбомы</label>
+                                {/* checked массив альбомов */}
+                                <SelectAlbum albums={form.arAlbums} func={ChangeSelectAlbums}/>
+                            </div>
+
+                            <div className="mb-3">
+                                <label htmlFor="inputTitle" className="form-label">Название</label>
+                                <input type="text" className="form-control" id="inputTitle"
+                                       onChange={onChangeText} value={form.inputTitle}/>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="inputText" className="form-label">Описание</label>
+                                <textarea className="form-control" id="inputText" rows="5"
+                                          onChange={onChangeText} value={form.inputText}></textarea>
+                            </div>
+
+                            {((form.processBar >0) && (form.processBar <100)) ? <div className="mb-3"><p className="text-primary">Загружаю</p></div>:null}
+                            {(form.processBar === 100) ? <div className="mb-3"><p className="text-success">Загружено</p></div>:null}
+                            <div className="progress">
+                                <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style={{width: `${form.processBar}%`}} aria-valuenow={form.processBar} aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal"
+                                    onClick={onFormClose}>Закрыть
+                            </button>
+                            <button type="submit" className="btn btn-primary"
+                                    disabled={(form.processBar !== 0) ? true : false}>Добавить
+                            </button>
+                        </div>
                     </div>
-                </form>
-            </div>
-        )
-    }
 
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default connect (
