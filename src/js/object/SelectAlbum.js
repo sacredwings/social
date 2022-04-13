3a4bff0f44a62b91a3fd079923f1d40902b5d10e
stronggeniusa@gmail.com <stronggeniusa@gmail.com>
2022-04-13 16:24:59 +0700

    Два уровня в альбомах

--- src/js/object/SelectAlbum.js
+++ src/js/object/SelectAlbum.js
@@ -14,19 +14,30 @@ export default function ({albums, module, user_id, group_id, func}) {
 
     //загрузка альбомов произошла
     useEffect (()=>{
-        //альбомы для отображения
+        //поиск по большему количеству альбомов - всем загруженым
         let rsAlbums = download.map((item, i)=>{
-            for (let itemOld of old) {
-                if (itemOld._id === item._id)
+            //обнуляем чек по умолчанию
+            item.checked = false
+            //ищем в среди установленных
+            old.forEach((olbItem, oldI)=>{
+                if (olbItem._id === item._id)
                     item.checked = true
-            }
+            })
+
+            item._album_id = item._album_id.map((subItem, subI)=>{
+                subItem.checked = false
+                old.forEach((olbItem, oldI)=>{
+                    if (olbItem._id === subItem._id)
+                        subItem.checked = true
+                })
+                return subItem
+            })
+
             return item
         })
 
         //сохраняем в state для контроля checked
         setChecked(rsAlbums)
-
-        console.log(rsAlbums)
     }, [download])
 
     //загрузка альбомов
@@ -58,37 +69,58 @@ export default function ({albums, module, user_id, group_id, func}) {
 
     //меняем свойство checked у элемента в state
     function onChangeChecked (id) {
+        //перед поиском обнуляем
+        let arId = []
+
         //поиск нужного элемента в массиве
         let newChecked = checked.map((item, i)=>{
+            //ищем в 1 уровне
             if (item._id === id) item.checked = !item.checked
-            return item
-        })
 
-        //оставляем только нужные элементы из массива
-        let newResult = newChecked.filter((item, i)=>{
-            if (item.checked === true) return item._id
-        })
-        //достаем их id
-        newResult = newResult.map((item, i)=>{
-            if (item.checked === true) return item._id
+            item._album_id = item._album_id.map((subItem, subI)=>{
+                //ищем в 2 уровне
+                if (subItem._id === id) subItem.checked = !subItem.checked
+
+                if (subItem.checked) arId.push(subItem._id)
+                return subItem
+            })
+
+            if (item.checked) arId.push(item._id)
+            return item
         })
 
         //сохраняем изменения в state для контроля checked
         setChecked(newChecked)
 
         //выводим результат
-        func(newResult)
+        func(arId)
+    }
+
+    const Level2 = (items) => {
+        return items.map((item, i)=>{
+            return <>
+                <div className={`form-check`} key={i}>
+                    <input className="form-check-input" type="checkbox" checked={item.checked} onChange={() => onChangeChecked(item._id)}/>
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        --- {item.title}
+                    </label>
+                </div>
+            </>
+        })
     }
 
     return (
         <>
             {checked.map((item, i)=>{
-                return <div className="form-check" key={i}>
-                    <input className="form-check-input" type="checkbox" checked={item.checked} onChange={() => onChangeChecked(item._id)}/>
-                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                        {item.title}
-                    </label>
-                </div>
+                return <>
+                    <div className="form-check" key={i}>
+                        <input className="form-check-input" type="checkbox" checked={item.checked} onChange={() => onChangeChecked(item._id)}/>
+                        <label className="form-check-label" htmlFor="flexCheckDefault">
+                            {item.title}
+                        </label>
+                    </div>
+                    {(item._album_id) ? Level2(item._album_id) : null}
+                </>
             })}
         </>
     );
